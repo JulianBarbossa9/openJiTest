a23707fff973ac25d83cfef68bd55ec5293e3e89
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-18 23:01:09 -0500

    add provider into app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { darkTheme, lightTheme } from '../themes'
+import UIProvider from '../context/ui/UIProvider'
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   )
 }
 
