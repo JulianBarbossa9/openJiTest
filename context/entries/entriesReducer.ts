4b2bbabed9c73d0b7e63e287e4a67b123e9cead8
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-27 11:19:04 -0500

    create new case type

--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -4,6 +4,7 @@ import { EntriesState } from './EntriesProvider';
 
 type EntriesActionType = 
 | { type: '[Entry] - Add new Entry', payload: entry}
+| { type: '[Entry] - Entry Update', payload: entry}
 
 export const entriesReducer = (state: EntriesState, action: EntriesActionType): EntriesState => {
 
@@ -12,6 +13,16 @@ switch(action.type) {
     return {
       ...state,
       entries: [...state.entries, action.payload]
+    }
+  case '[Entry] - Entry Update':
+    return {
+      ...state,
+      entries: state.entries.map(entry => {
+        if (entry._id === action.payload._id){
+          entry.status = action.payload.status
+        }
+        return entry
+      })
     }
 
   default:
