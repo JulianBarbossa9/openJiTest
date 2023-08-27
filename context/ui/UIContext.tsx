804fe2d79bbfc162ff6ce666886052b241c2cfbb
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-26 22:44:07 -0500

    add new methods in context

--- context/ui/UIContext.tsx
+++ context/ui/UIContext.tsx
@@ -4,11 +4,14 @@ import { createContext } from 'react';
 interface ContexProps {
      sideMenuOpen: boolean
      isAddingEntry: boolean
+     isDragging: boolean
 
      //Methods
      openSideMenu: () =>void
      closeSideMenu: () =>void
      setIsAddingEntry: (isAdding: boolean) => void
+     startDragging: () => void
+     endDragging: () => void
 }
 
 export const UIContext = createContext({} as ContexProps);
