aab9cbd433588d2652315d2cc6bceeb2b6652190
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-26 22:44:44 -0500

    add new types when start an finish drag

--- context/ui/uiReducer.ts
+++ context/ui/uiReducer.ts
@@ -1,3 +1,4 @@
+
 import { UIState } from './UIProvider';
 
 
@@ -5,6 +6,8 @@ type UIActionType =
 | { type: '[UI] - Open SideBar'}
 | { type: '[UI] - Close SideBar'}
 | { type: '[UI] - Is Adding Entry', payload: boolean }
+| { type: '[UI] - Start Dragging' }
+| { type: '[UI] - End Dragging' }
 
 export const uiReducer = (state: UIState, action: UIActionType): UIState => {
 
@@ -23,6 +26,16 @@ switch(action.type) {
     return {
       ...state,
       isAddingEntry: action.payload,
+    }
+  case '[UI] - Start Dragging':
+    return {
+      ...state,
+      isDragging: true,
+    }
+  case '[UI] - End Dragging':
+    return {
+      ...state,
+      isDragging: false,
     }
 
   default:
