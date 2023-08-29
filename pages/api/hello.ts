5b8affd1fa0aa95be6f9c69e29f29fb695d6030a
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-29 10:55:29 -0500

    update type in api/hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,12 +2,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  ok: boolean,
+  message: string,
+  method: string,
+  // secret?: string,
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  // console.log(process.env)
+  // Here we can change the status
+  res.status(200).json({
+    ok: true,
+    message: 'All good ',
+    // with this I know the data (see in postman)
+    method: req.method || 'no data',
+    // secret: process.env.SECRET_KEY
+  })
 }
