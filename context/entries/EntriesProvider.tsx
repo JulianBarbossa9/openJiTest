a2693fa44af04171736b056b9d2eac89f0ff2a3c
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-30 14:17:00 -0500

    update form to add new entry

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -1,8 +1,9 @@
-import { FC, useReducer } from 'react';
+import { FC, useEffect, useReducer } from 'react';
 import { EntriesContext } from './EntriesContext';
 import { v4 as uuid4 } from 'uuid'
 import { entriesReducer } from './entriesReducer';
 import { entry } from '../../interfaces';
+import { entriesApi } from '../../apis';
 
 
 export interface EntriesState {
@@ -24,20 +25,39 @@ const EntriesProvider:React.FC<Props> = ({ children }) => {
 
  const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE)
 
- const addNewEntry = (description: string ) => {
-    const newEntry: entry = {
-      _id: uuid4(),
-      description: description,
-      createdAt: Date.now(),
-      status: 'pending'
-    }
+ const addNewEntry = async(description: string ) => {
 
-    dispatch({ type: '[Entry] - Add new Entry', payload: newEntry})
+  try {
+    const { data } = await entriesApi.post<entry>('/entries', { description: description})
+    dispatch({ type: '[Entry] - Add new Entry', payload: data})
+    
+  } catch (error) {
+    console.log(error)
+  }
+    
+  // const newEntry: entry = {
+  //     _id: uuid4(),
+  //     description: description,
+  //     createdAt: Date.now(),
+  //     status: 'pending'
+  //   }
+
  }
 
  const updateEntry = ( entry: entry) => {
   dispatch({type: '[Entry] - Entry Update', payload: entry})
  }
+
+ const refreshEntries =async () => {
+  const { data } = await entriesApi.get<entry[]>('/entries')
+  // console.log(response)
+  dispatch({ type: '[Entry] - Refresh Data', payload: data})
+
+ }
+
+ useEffect(() => {
+  refreshEntries()
+ },[])
   
 
   return (
