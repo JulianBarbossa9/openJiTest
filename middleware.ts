3d6d5aa5daff00e7dacd6ad890cf5690ad584723
Julian Barbosa <julianbarbossa@hotmail.com>
2023-08-31 22:20:58 -0500

    update middelware for next 13

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,23 @@ import type { NextRequest } from 'next/server'
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   
-  console.log({request: request.nextUrl })
+  
+  if (request.nextUrl.pathname.startsWith('/api/entries/')) {
+    const id = request.nextUrl.pathname.replace('/api/entries/', '')
+    const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+    if ( !checkMongoIDRegExp.test(id)){
+      return new NextResponse(
+        JSON.stringify({
+          succes: false,
+          message: `${id} is not valid MongoID`
+        }),
+        {
+          status: 400, headers: { 'content-type': 'application/json'}
+        }
+      )
+    }
+  }
+  // console.log({request: request.nextUrl })
   return NextResponse.next()
 }
  
@@ -12,7 +28,7 @@ export function middleware(request: NextRequest) {
 export const config = {
   // matcher: '/about/:path*',
   matcher: [ 
-    // '/api/:path',
-    '/api/entries/:path'
+    // '/api/:path', 
+    '/api/entries/:path*'
   ]
 }
