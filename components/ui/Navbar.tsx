241c2a92db691af4038af24692b816a201216900
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-01 13:42:27 -0500

    update navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,7 +1,8 @@
-import { AppBar, IconButton, Toolbar, Typography } from '@mui/material'
+import { AppBar, IconButton, Link, Toolbar, Typography } from '@mui/material'
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined'
 import React, { useContext } from 'react'
 import { UIContext } from '../../context/ui'
+import NextLink from 'next/link'
 
 const NavBar = () => {
   
@@ -17,7 +18,11 @@ const NavBar = () => {
         >
           <MenuOutlinedIcon />
         </IconButton>
-        <Typography variant='h6'>OpenJira</Typography>
+        <NextLink href="/" passHref legacyBehavior>
+          <Link underline='none' color='white' >
+            <Typography variant='h6'>OpenJira</Typography>
+          </Link>
+        </NextLink>
       </Toolbar>
     </AppBar>
   )
