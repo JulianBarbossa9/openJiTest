b29ab6988303d16b74e86f4cc23c6e5c2ed4cf03
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-05 23:01:07 -0500

    update method delete in the context

--- context/entries/EntriesContext.tsx
+++ context/entries/EntriesContext.tsx
@@ -7,7 +7,8 @@ interface ContextProps {
 
      //Methods
      addNewEntry: (description: string) => void 
-     updateEntry: (entry: entry) => void
+     updateEntry: (entry: entry, showSnackbar?: boolean | undefined ) => void
+     deleteEntry: (_id: string, showSnackbar?: boolean | undefined ) => void
 }
 
 export const EntriesContext = createContext({} as ContextProps);
