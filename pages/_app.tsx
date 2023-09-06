1cc00052bd84532042288f3e684087452a951247
Julian Barbosa <julianbarbossa@hotmail.com>
2023-09-05 23:03:35 -0500

    add snackprovider component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,24 @@
-import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { darkTheme, lightTheme } from '../themes'
-import UIProvider from '../context/ui/UIProvider'
-import EntriesProvider from '../context/entries/EntriesProvider'
-
-
+import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { darkTheme, lightTheme } from "../themes";
+import UIProvider from "../context/ui/UIProvider";
+import EntriesProvider from "../context/entries/EntriesProvider";
+import { SnackbarProvider } from "notistack";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <EntriesProvider>
-      <UIProvider>
-        <ThemeProvider theme={darkTheme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-        </ThemeProvider>
-      </UIProvider>
-    </EntriesProvider>
-  )
+    <SnackbarProvider maxSnack={ 3 }>
+      <EntriesProvider>
+        <UIProvider>
+          <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UIProvider>
+      </EntriesProvider>
+    </SnackbarProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
